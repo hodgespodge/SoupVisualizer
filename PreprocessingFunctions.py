951ce5c5b13be3b741a89435a7b68dcf95e6bf46
hodgespodge <shodges@unca.edu>
2019-12-02 18:49:30 -0500

    Created file for holding audio processing functions

--- PreprocessingFunctions.py
+++ PreprocessingFunctions.py
@@ -8,3 +8,12 @@ np.set_printoptions(precision=3,edgeitems=5,suppress=True,linewidth=50)
 
 def find_fundamental_frequency_of_tone(freqs, times, spectro):
     return spectro
+
+'''
+As far as I can tell, spacing on harmonics determins the fundemental frequency.
+The challenge then is in identifying the spacing, finding the fundemental frequency, 
+then elimating the other tones. 
+
+
+
+'''
