a33f5de261c29a1e9f429890f39ae99cc12f54f8
Samuel Hodges <shodges@unca.edu>
2020-04-14 16:20:10 -0400

    began work on trying to cluster vocal segments.

--- PreprocessingFunctions.py
+++ PreprocessingFunctions.py
@@ -52,6 +52,9 @@ def create_new_vocal_profile(rate,vocal_amplitude,display_interval_ms,song_name)
     crepe_vocal_time, crepe_vocal_frequency, crepe_vocal_confidence, crepe_vocal_activation = \
         crepe.predict(audio=vocal_amplitude,sr=rate,step_size=display_interval_ms,viterbi=True)
 
+    crepe_vocal_confidence= adjust_vocal_onset_confidence(window_size=20, threshold=0.75, crepe_vocal_frequency=crepe_vocal_frequency,
+                                  crepe_vocal_confidence=crepe_vocal_confidence, crepe_vocal_time=crepe_vocal_time)
+
     # first empty the file if it exists already
     pickle_output = open("pickles/" + song_name + "_crepe.pickle", "wb")
     pickle_output.close()
@@ -78,44 +81,31 @@ def wav_to_mp3(songpath,outpath):
 def create_new_ellipse_profile():
     return None
 
+def slope(X, Y):
+
+    Y = list(Y)
+
+    xbar = sum(X) / len(X)
+    ybar = sum(Y) / len(Y)
+    n = len(X)  # or len(Y)
+
+    numer = sum([xi * yi for xi, yi in zip(X, Y)]) - n * xbar * ybar
+    denum = sum([xi ** 2 for xi in X]) - n * xbar ** 2
+
+    b = numer / denum
+
+    return b
+
 def adjust_vocal_onset_confidence(window_size, threshold,crepe_vocal_frequency,crepe_vocal_confidence,crepe_vocal_time):
 
-
-
-    def slope(X, Y):
-
-        # print("t",X)
-        # print("con",Y)
-
-        Y = list(Y)
-
-        xbar = sum(X) / len(X)
-        ybar = sum(Y) / len(Y)
-        n = len(X)  # or len(Y)
-
-        numer = sum([xi * yi for xi, yi in zip(X, Y)]) - n * xbar * ybar
-        denum = sum([xi ** 2 for xi in X]) - n * xbar ** 2
-
-        b = numer / denum
-
-        return b
-
-    print("slope test",slope([0,1,2],[2,3,4]))
-    print("slope test", slope([0, 1, 2], [4, 3, 2]))
-
-    import math
-    import pylab
-
-    # window_size = math.floor(window_size / 2)
-
-    for i in range(len(crepe_vocal_frequency)):
+    for i in range(len(crepe_vocal_confidence)):
         frequency = crepe_vocal_frequency[i : i + window_size ]
         confidence = crepe_vocal_confidence[i : i +  window_size ]
 
         average_frequency = 0
         average_confidence = 0
 
-        if len(frequency) > 0:
+        if len(confidence) > 0:
 
             average_frequency = sum(frequency)/len(frequency)
             average_confidence = sum(confidence)/len(confidence)
@@ -123,24 +113,78 @@ def adjust_vocal_onset_confidence(window_size, threshold,crepe_vocal_frequency,c
         con_slope = slope(crepe_vocal_time[i : i + window_size], confidence)
 
         if average_confidence > threshold:
-            print(crepe_vocal_time[i], str(round(average_frequency, 2)),str(round(average_confidence, 2))," <---------@@")
-            crepe_vocal_confidence[i] = 1
+            # print(crepe_vocal_time[i], str(round(average_frequency, 2)),str(round(average_confidence, 2))," <---------@@")
 
+            #rounds the vocal confidence so animations are less eratic
+            crepe_vocal_confidence[i] = average_confidence
 
         elif con_slope - (2/(3*average_confidence * average_confidence)) > threshold  :
 
-            crepe_vocal_confidence[i] = 0.8
+            #updates vocal confidence to better match steep onsets
+            crepe_vocal_confidence[i] = threshold + 0.1*(average_confidence * average_confidence* con_slope)%10
 
-            print(crepe_vocal_time[i],str(round(average_frequency, 2)),str(round(average_confidence, 2))," <-------",str(round(con_slope, 2)),str(round(2/(3*average_confidence * average_confidence), 2)))
+            # print(crepe_vocal_time[i],str(round(average_frequency, 2)),str(round(average_confidence, 2))," <-------",str(round(con_slope, 2)),str(round(crepe_vocal_confidence[i], 2)))
 
         else:
-            print(crepe_vocal_time[i],str(round(average_frequency, 2)),str(round(average_confidence, 2)))
 
-        # if len(frequency) > 0:
-        #     vocal_slope = slope(range(window_size),confidence)[0]
-        #     if vocal_slope > 0.5 and average_confidence > threshold - 0.2:
-        #         # print(str(round(vocal_slope, 2)))
+            crepe_vocal_confidence[i] = average_confidence
 
+            # print(crepe_vocal_time[i],str(round(average_frequency, 2)),str(round(average_confidence, 2)))
+
+    return crepe_vocal_confidence
+
+def group_vocals(window_size, threshold,crepe_vocal_frequency,crepe_vocal_confidence,crepe_vocal_time):
+
+
+    time_between_distinct_clusters = 1
+    vocal_times = []
+    last_vocal_time = -1000
+    vocal_bin = []
+    min_vocal_segment_length = 1
+
+    for i in range(len(crepe_vocal_time)):
+
+        if crepe_vocal_confidence[i] > threshold:
+
+            if crepe_vocal_time[i] - last_vocal_time > time_between_distinct_clusters:
+                vocal_times.append(vocal_bin)
+                vocal_bin = []
+
+            vocal_bin.append(crepe_vocal_time[i])
+            last_vocal_time = crepe_vocal_time[i]
+
+    vocal_times.pop(0)
+
+    #TEST
+    vocal_times.append([84.5,85.4])
+
+    for item in vocal_times:
+        print(item[0], item[-1])
+
+    for i in range(len(vocal_times)):
+
+        #if the vocal segment is too short, try and append it to another vocal segment.
+        if(vocal_times[i][-1] - vocal_times[i][0]) < min_vocal_segment_length:
+
+            distance_to_lower = vocal_times[i][0] - vocal_times[i -1][-1]
+            distance_to_upper = vocal_times[i + 1][0] -  vocal_times[i][-1]
+
+            print(distance_to_lower,distance_to_upper)
+
+            if distance_to_lower > distance_to_upper:
+                pass
+
+            print(vocal_times[0],vocal_times[-1])
+
+
+
+
+
+
+
+        # print(item[0],item[-1])
+
+        # print(item)
 
 import os
 from scipy.io.wavfile import read
@@ -187,4 +231,7 @@ except:
     crepe_vocal_time, crepe_vocal_frequency, crepe_vocal_confidence, crepe_vocal_activation = \
         create_new_vocal_profile(rate, vocal_amplitude, display_interval_ms, song_name)
 
-custom_threshold_smoother(window_size=20,threshold=0.75,crepe_vocal_frequency=crepe_vocal_frequency,crepe_vocal_confidence=crepe_vocal_confidence,crepe_vocal_time=crepe_vocal_time)
+crepe_vocal_confidence =  adjust_vocal_onset_confidence(window_size=20,threshold=0.75,crepe_vocal_frequency=crepe_vocal_frequency,crepe_vocal_confidence=crepe_vocal_confidence,crepe_vocal_time=crepe_vocal_time)
+
+group_vocals(window_size=20,threshold=0.75,crepe_vocal_frequency=crepe_vocal_frequency,crepe_vocal_confidence=crepe_vocal_confidence,crepe_vocal_time=crepe_vocal_time)
+
